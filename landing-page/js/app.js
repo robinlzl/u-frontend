4254872ee4098ae6166d20fba57b39a1250267a9
RL858K <rl858k@us.att.com>
2020-02-23 09:10:55 -0500

    add js

--- landing-page/js/app.js
+++ landing-page/js/app.js
@@ -34,7 +34,29 @@
 */
 
 // build the nav
+let defaultActiveSection = null
+const buildMenu = function() {
 
+    const sections = document.getElementsByTagName('section')
+    console.log(`section ${sections.length}`)
+    const ulElement = document.getElementById('navbar__list')
+    if (sections.length > 0) {
+        defaultActiveSection = sections[0]
+    }
+    for (let i = 0; i < sections.length; i++) {
+        const liElement = document.createElement('li')
+        liElement.innerHTML = sections[i].getAttribute('data-nav')
+        ulElement.appendChild(liElement)
+        liElement.addEventListener('click', evt => {
+            const rect = sections[i].getBoundingClientRect()
+            window.scrollTo(rect.left + window.scrollX, rect.top + window.scrollY)
+            sections[i].classList.toggle("your-active-class")
+            defaultActiveSection.classList.toggle("your-active-class")
+            defaultActiveSection = sections[i]
+        })
+    }
+
+}
 
 // Add class 'active' to section when near top of viewport
 
@@ -49,6 +71,7 @@
 */
 
 // Build menu 
+buildMenu()
 
 // Scroll to section on link click
 
